3cf1e58c8b252882a9781f455e7432d0c8698791
djkaif <moviedj14@gmail.com>
2026-03-05 12:29:53 +0600

    Added def speedtest

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask 
 app = Flask(__name__)
 
+def speedtest():
+  st = speedtest.speedtest()
+  st.get_best_server()
+
+  download_bps = st.download()
+  upload_bps = st.upload()
+  ping = st.result.ping
+
+  download_mbs = round(download_bps / 10**6,2)
+  upload_mbs = round(upload_bps / 10**6, 2)
+  return{
+    "download": download_mbs,
+    "upload": upload_mbs,
+    "ping": ping,
+}
+
 @app.route('/')
 def index():
   return "Server is running!"
 
 if __name__ == '__main__':
+  print("Starting..")
+  print("Testing speed... please wait (this takes a few seconds)...")
+  results = speedtest()
+  prinf(f"Speed test done!\nResult: {results}")
   app.run(debug=True)
