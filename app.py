f6c8d20761d97139bdc22d53cf3a939d286ad048
djkaif <moviedj14@gmail.com>
2026-03-05 12:33:37 +0600

    Fixed speedtest functiom Fixed 2 spelling mistake

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask 
 app = Flask(__name__)
 
-def speedtest():
-  st = speedtest.speedtest()
+def speedcheck():
+  st = speedtest.Speedtest()
   st.get_best_server()
 
   download_bps = st.download()
   upload_bps = st.upload()
-  ping = st.result.ping
+  ping = st.results.ping
 
   download_mbs = round(download_bps / 10**6,2)
   upload_mbs = round(upload_bps / 10**6, 2)
@@ -24,6 +24,6 @@ def index():
 if __name__ == '__main__':
   print("Starting..")
   print("Testing speed... please wait (this takes a few seconds)...")
-  results = speedtest()
+  results = speedcheck()
   prinf(f"Speed test done!\nResult: {results}")
   app.run(debug=True)
